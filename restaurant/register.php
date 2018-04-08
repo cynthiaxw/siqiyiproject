09f6f35f1ca228fe1a6770ea377fd1f0f80ece5a
cynthiaxw <365683780@qq.com>
2018-04-08 09:34:30 +0800

    Update register.php

--- restaurant/register.php
+++ restaurant/register.php
@@ -1,19 +1,54 @@
 <?php include('server.php') ?>
+<?php
+
+
+	// REGISTER USER
+	if (isset($_POST['reg_user'])) {
+		// receive all input values from the form
+		$username = mysqli_real_escape_string($db, $_POST['username']);
+		$email = mysqli_real_escape_string($db, $_POST['email']);
+		$email = mysqli_real_escape_string($db, $_POST['phone']);
+		$password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+		$password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+		// form validation: ensure that the form is correctly filled
+		if (empty($username)) { array_push($errors, "Username is required"); }
+		if (empty($email)) { array_push($errors, "Email is required"); }
+		if (empty($phone)) { array_push($phone, "Phone is required"); }
+		if (empty($password_1)) { array_push($errors, "Password is required"); }
+
+		if ($password_1 != $password_2) {
+			array_push($errors, "The two passwords do not match");
+		}
+
+		// register user if there are no errors in the form
+		if (count($errors) == 0) {
+			$password = md5($password_1);//encrypt the password before saving in the database
+			$query = "INSERT INTO users (username, email, phone, password)
+					  VALUES('$username', '$email', '$password')";
+			mysqli_query($db, $query);
+
+			$_SESSION['username'] = $username;
+			$_SESSION['success'] = "You are now logged in";
+			header('location: homepage.php');
+		}
+
+	}
+
+ ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Registration system PHP and MySQL</title>
+	<title>Restaurant Reservation System</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 	<div class="header">
 		<h2>Register</h2>
 	</div>
-	
+
 	<form method="post" action="register.php">
-
 		<?php include('errors.php'); ?>
-
 		<div class="input-group">
 			<label>Username</label>
 			<input type="text" name="username" value="<?php echo $username; ?>">
@@ -21,21 +56,18 @@
 		<div class="input-group">
 			<label>Email</label>
 			<input type="email" name="email" value="<?php echo $email; ?>">
-		</div>
-		<div class="input-group">
+			<label>Phone</label>
+			<input type="phone" name="phone" value="<?php echo $phone; ?>">
 			<label>Password</label>
 			<input type="password" name="password_1">
-		</div>
-		<div class="input-group">
 			<label>Confirm password</label>
 			<input type="password" name="password_2">
+			<button type="submit" class="button" name="reg_user">Register</button>
 		</div>
-		<div class="input-group">
-			<button type="submit" class="btn" name="reg_user">Register</button>
-		</div>
-		<p>
+		<center><p>
 			Already a member? <a href="login.php">Sign in</a>
-		</p>
+			<br/><a href="homepage.php">Back to home page.</a>
+		</p><center/>
 	</form>
 </body>
 </html>
